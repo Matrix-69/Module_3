a4c3635fe3d6e8c902a6221e803292d2b728beb4
Matrix-69 <tarunreddy5432@gmail.com>
2024-07-10 11:44:44 +0530

    10.07.2024

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+$servername="localhost";
+$username="gcu";
+$password="123456";
+$dbname="engg";
+// create connection
+$conn = new mysqli($servername, $username, $password, $dbname)
+// check connection
+if($conn->connect_error){
+    die("Connection Failed: ". $conn->connect_error);
+}
+echo"Connected successfully!";
+$conn->close();
+?>
---
+++ php1.php
@@ -0,0 +1,8 @@
+<?php
+echo "Welcome to PHP"
+?>
+
+<?php
+//to open file use - localhost/Module_3/php1.php
+//*this is just note
+?>
